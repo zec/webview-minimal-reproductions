fae710b079f6afac8f5f82eccbdeb5787eb520fa
Zachary Catlin <z@zc.is>
2021-02-15 11:49:01 -0500

    Fleshed out template project; applied Unlicense to this repo

--- template/src/main.rs
+++ template/src/main.rs
@@ -1,3 +1,35 @@
+/*
+   2021 Zachary Catlin;
+   avaliable under the terms of the Unlicense (see ../../UNLICENSE)
+*/
+
+extern crate web_view;
+
+use web_view::WVResult;
+
 fn main() {
-    println!("Hello, world!");
+    let result = run_webview();
+
+    if let Err(ref e) = result {
+        eprintln!("Error in building or running WebView: {}", e);
+        std::process::exit(1);
+    }
+}
+
+const HTML_CONTENT: &'static str = include_str!("../content.html");
+
+fn run_webview() -> WVResult<()> {
+    use web_view::*;
+
+    let mut wv = WebViewBuilder::new()
+        .title("Bug reproduction example")
+        .content(Content::Url("data:text/plain,%20"))
+        .user_data(())
+        .invoke_handler(|_wv, _arg| { Ok(())})
+        .build()?;
+
+    wv.set_html(HTML_CONTENT)?;
+    wv.run()?;
+
+    Ok(())
 }
